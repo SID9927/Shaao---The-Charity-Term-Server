611b724836988cd31df40ab80474581e9413b4c1
Siddharth <5065sid@gmail.com>
2025-10-18 16:59:11 +0530

    .

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -8,9 +8,7 @@ dotenv.config();
 // console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT),
-  secure: false,
+  service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
