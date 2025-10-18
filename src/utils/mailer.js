6d89da364e0a477bd390376d0717889c8e8ce425
SIDDHARTH <143601354+SID9927@users.noreply.github.com>
2025-10-19 00:18:34 +0530

    Update mailer.js

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -8,7 +8,9 @@ dotenv.config();
 // console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
